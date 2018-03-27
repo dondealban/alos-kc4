9b110d0517a04b08e4c1ae13f0cd0585734c407f
dondealban <dondealban@gmail.com>
2018-03-27 18:39:48 +0800

    Added lines for test plotting parameters

--- scripts/R/UAPA.Boxplots.LandsatSAR.R
+++ scripts/R/UAPA.Boxplots.LandsatSAR.R
@@ -13,3 +13,20 @@ setwd("/Users/dondealban/KC4/")
 # Load Required Packages -----------------
 
 library(ggplot2)
+
+
+# Read Input Data ------------------------
+
+uapal1 <- read.csv(file="UAPA_L1.csv", header=TRUE, sep=",")
+uapal2 <- read.csv(file="UAPA_L2.csv", header=TRUE, sep=",")
+uapal3 <- read.csv(file="UAPA_L3.csv", header=TRUE, sep=",")
+uapal4 <- read.csv(file="UAPA_L4.csv", header=TRUE, sep=",")
+
+
+# Generate Plots -------------------------
+
+plotL1 <- ggplot() + geom_boxplot(aes(y=UA, x=CATEGORY, fill=SITE), data=uapal1, outlier.shape = 1, outlier.size = 1)
+plotL1 <- plotL1 + labs(x="Land Cover Category", y="Accuracy (x 100%)")
+plotL1 <- plotL1 + scale_y_continuous()
+plotL1 <- plotL1 + theme(axis.text.x = element_text(angle=90, hjust=1))
+
