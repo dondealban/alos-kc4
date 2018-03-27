67328787626aa0a523c252b3a6252bad5b1bc3a8
dondealban <dondealban@gmail.com>
2018-03-27 19:51:54 +0800

    Added lines for manipulating data for other classification levels

--- scripts/R/UAPA.Boxplots.LandsatSAR.R
+++ scripts/R/UAPA.Boxplots.LandsatSAR.R
@@ -25,19 +25,46 @@ uapal4 <- read.csv(file="UAPA_L4.csv", header=TRUE, sep=",")
 
 # Manipulate Data ------------------------
 
-
 # Convert wide format data frame into long format data frame
 ml1 <- melt(uapal1, id.vars=c("CATEGORY","SITE","LEVEL","YEAR"))
+ml2 <- melt(uapal2, id.vars=c("CATEGORY","SITE","LEVEL","YEAR"))
+ml3 <- melt(uapal3, id.vars=c("CATEGORY","SITE","LEVEL","YEAR"))
+ml4 <- melt(uapal4, id.vars=c("CATEGORY","SITE","LEVEL","YEAR"))
 
-
-
-# rename column names
+# Rename column names
 colnames(ml1) <- c("Category","Site","Level","Year","Acc.Type","Acc.Value")
+colnames(ml2) <- c("Category","Site","Level","Year","Acc.Type","Acc.Value")
+colnames(ml3) <- c("Category","Site","Level","Year","Acc.Type","Acc.Value")
+colnames(ml4) <- c("Category","Site","Level","Year","Acc.Type","Acc.Value")
+
+# Replace zeroes with NA
+ml1[ml1==0] <- NA
+ml2[ml2==0] <- NA
+ml3[ml3==0] <- NA
+ml4[ml4==0] <- NA
 
 # Generate Plots -------------------------
 
 plotL1 <- ggplot() + geom_boxplot(aes(y=Acc.Value, x=Category, fill=Acc.Type), data=ml1, outlier.shape = 1, outlier.size = 1)
 plotL1 <- plotL1 + labs(x="Land Cover Category", y="Accuracy (x 100%)")
+plotL1 <- plotL1 + scale_fill_manual(values=c("light blue","light green"), name="", labels=c("UA","PA"))
 plotL1 <- plotL1 + scale_y_continuous()
+plotL1
 
+plotL2 <- ggplot() + geom_boxplot(aes(y=Acc.Value, x=Category, fill=Acc.Type), data=ml2, outlier.shape = 1, outlier.size = 1)
+plotL2 <- plotL2 + labs(x="Land Cover Category", y="Accuracy (x 100%)")
+plotL2 <- plotL2 + scale_fill_manual(values=c("light blue","light green"), name="", labels=c("UA","PA"))
+plotL2 <- plotL2 + scale_y_continuous()
+plotL2
 
+plotL3 <- ggplot() + geom_boxplot(aes(y=Acc.Value, x=Category, fill=Acc.Type), data=ml3, outlier.shape = 1, outlier.size = 1)
+plotL3 <- plotL3 + labs(x="Land Cover Category", y="Accuracy (x 100%)")
+plotL3 <- plotL3 + scale_fill_manual(values=c("light blue","light green"), name="", labels=c("UA","PA"))
+plotL3 <- plotL3 + scale_y_continuous()
+plotL3
+
+plotL4 <- ggplot() + geom_boxplot(aes(y=Acc.Value, x=Category, fill=Acc.Type), data=ml4, outlier.shape = 1, outlier.size = 1)
+plotL4 <- plotL4 + labs(x="Land Cover Category", y="Accuracy (x 100%)")
+plotL4 <- plotL4 + scale_fill_manual(values=c("light blue","light green"), name="", labels=c("UA","PA"))
+plotL4 <- plotL4 + scale_y_continuous()
+plotL4
