208221b07ca7ec01471e4f05b511a8d22b3de313
dondealban <dondealban@gmail.com>
2018-03-27 18:28:56 +0800

    Initial commit for new script

---
+++ scripts/R/UAPA.Boxplots.LandsatSAR.R
@@ -0,0 +1,15 @@
+# This script produces boxplots to visualise the user's and producer's accuracies obtained
+# for each land cover category at a particular classification level. The accuracies of each 
+# category is plotted at four classification levels.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   27 Mar 2018
+# Last Modified:  
+
+# Set Working Directory ------------------
+
+setwd("/Users/dondealban/KC4/")
+
+# Load Required Packages -----------------
+
+library(ggplot2)
